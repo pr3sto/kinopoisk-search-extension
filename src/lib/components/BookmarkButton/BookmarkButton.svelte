88c53119a67bf8fce2357a80a24c4b0651baa17a
Alexey Chirukhin <pr3sto1377@gmail.com>
2025-07-09 02:15:16 +0300

    optimize SVGs

--- src/lib/components/BookmarkButton/BookmarkButton.svelte
+++ src/lib/components/BookmarkButton/BookmarkButton.svelte
@@ -155,7 +155,7 @@
     height: 100%;
     border: none;
     background: $font-color-light;
-    mask: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3Csvg viewBox='0 0 330 330' xmlns='http://www.w3.org/2000/svg'%3E%3Cg%3E%3Ctitle%3Ebackground%3C/title%3E%3Crect x='-1' y='-1' width='802' height='602' fill='none'/%3E%3C/g%3E%3Cg%3E%3Ctitle%3ELayer 1%3C/title%3E%3Cpath d='m265 0h-200c-8.284 0-15 6.716-15 15v300c0 5.766 3.305 11.022 8.502 13.52s11.365 1.796 15.868-1.807l90.63-72.503 90.63 72.503c2.712 2.17 6.027 3.287 9.372 3.287 2.208 0 4.43-0.487 6.496-1.48 5.197-2.497 8.502-7.753 8.502-13.52v-300c0-8.284-6.716-15-15-15zm-15 283.79-75.63-60.503c-2.739-2.191-6.055-3.287-9.37-3.287s-6.631 1.096-9.37 3.287l-75.63 60.503v-253.79h170v253.79z'/%3E%3C/g%3E%3C/svg%3E%0A")
+    mask: url("data:image/svg+xml,%3Csvg viewBox='0 0 330 330' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill='none' d='M-1-1h802v602H-1z'/%3E%3Cpath d='M265 0H65c-8.284 0-15 6.716-15 15v300a15 15 0 0 0 24.37 11.713L165 254.21l90.63 72.503a15 15 0 0 0 9.372 3.287A15 15 0 0 0 280 315V15c0-8.284-6.716-15-15-15m-15 283.79-75.63-60.503c-2.739-2.191-6.055-3.287-9.37-3.287s-6.631 1.096-9.37 3.287L80 283.79V30h170z'/%3E%3C/svg%3E")
       center no-repeat;
     transition: background 0.1s ease-out;
     cursor: pointer;
@@ -167,7 +167,7 @@
 
     &:disabled {
       background: $accent-color;
-      mask: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3Csvg viewBox='0 0 330 330' xmlns='http://www.w3.org/2000/svg'%3E%3Cg%3E%3Ctitle%3Ebackground%3C/title%3E%3Crect x='-1' y='-1' width='802' height='602' fill='none'/%3E%3C/g%3E%3Cg%3E%3Ctitle%3ELayer 1%3C/title%3E%3Cpath d='m265 0h-200c-8.284 0-15 6.716-15 15v300c0 5.766 3.305 11.022 8.502 13.52s11.365 1.796 15.868-1.807l90.63-72.503 90.63 72.503c2.712 2.17 6.027 3.287 9.372 3.287 2.208 0 4.43-0.487 6.496-1.48 5.197-2.497 8.502-7.753 8.502-13.52v-300c0-8.284-6.716-15-15-15z' fill='%23f60'/%3E%3C/g%3E%3Cg%3E%3Ctitle%3Ebackground%3C/title%3E%3Crect x='-1' y='-1' width='332' height='332' fill='none'/%3E%3C/g%3E%3C/svg%3E%0A")
+      mask: url("data:image/svg+xml,%3Csvg viewBox='0 0 330 330' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill='none' d='M-1-1h802v602H-1z'/%3E%3Cpath d='M265 0H65c-8.284 0-15 6.716-15 15v300a15 15 0 0 0 24.37 11.713L165 254.21l90.63 72.503a15 15 0 0 0 9.372 3.287A15 15 0 0 0 280 315V15c0-8.284-6.716-15-15-15' fill='%23f60'/%3E%3Cpath fill='none' d='M-1-1h332v332H-1z'/%3E%3C/svg%3E")
         center no-repeat;
       cursor: default;
     }
@@ -181,7 +181,7 @@
     height: 100%;
     opacity: 0;
     z-index: 1;
-    background: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3Csvg viewBox='0 0 330 330' xmlns='http://www.w3.org/2000/svg'%3E%3Cg%3E%3Ctitle%3Ebackground%3C/title%3E%3Crect x='-1' y='-1' width='802' height='602' fill='none'/%3E%3C/g%3E%3Cg%3E%3Ctitle%3ELayer 1%3C/title%3E%3Cpath d='m265 0h-200c-8.284 0-15 6.716-15 15v300c0 5.766 3.305 11.022 8.502 13.52s11.365 1.796 15.868-1.807l90.63-72.503 90.63 72.503c2.712 2.17 6.027 3.287 9.372 3.287 2.208 0 4.43-0.487 6.496-1.48 5.197-2.497 8.502-7.753 8.502-13.52v-300c0-8.284-6.716-15-15-15z' fill='%23f60'/%3E%3C/g%3E%3Cg%3E%3Ctitle%3Ebackground%3C/title%3E%3Crect x='-1' y='-1' width='332' height='332' fill='none'/%3E%3C/g%3E%3C/svg%3E%0A")
+    background: url("data:image/svg+xml,%3Csvg viewBox='0 0 330 330' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill='none' d='M-1-1h802v602H-1z'/%3E%3Cpath d='M265 0H65c-8.284 0-15 6.716-15 15v300a15 15 0 0 0 24.37 11.713L165 254.21l90.63 72.503a15 15 0 0 0 9.372 3.287A15 15 0 0 0 280 315V15c0-8.284-6.716-15-15-15' fill='%23f60'/%3E%3Cpath fill='none' d='M-1-1h332v332H-1z'/%3E%3C/svg%3E")
       center no-repeat;
     animation: scale 0.7s cubic-bezier(0.26, 0.86, 0.63, 1);
     pointer-events: none;
